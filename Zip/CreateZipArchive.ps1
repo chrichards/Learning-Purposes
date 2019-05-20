2dfb1f01669bbd7c86297fee3152141196b676ec
Chris Richards <31672294+chrichards@users.noreply.github.com>
2019-05-20 10:50:38 -0700

    Update CreateZipArchive.ps1

--- Zip/CreateZipArchive.ps1
+++ Zip/CreateZipArchive.ps1
@@ -1,72 +1,82 @@
 Param(
   [Parameter(Mandatory=$true)]
-  [string]$source
+  [string]$source,
+
+  [Parameter(Mandatory=$true)]
+  [string]$destination
 )
 
-# Load required assemblies
-Add-Type -AssemblyName System.IO.Compression
-Add-Type -AssemblyName System.IO.Compression.FileSystem
+begin {
+    # Load required assemblies
+    Add-Type -AssemblyName System.IO.Compression
+    Add-Type -AssemblyName System.IO.Compression.FileSystem
 
-# Define paths
-$here = (Get-Item -Path .\).FullName
-$dest = "$source.zip"
+    # Set the compression level for the archive
+    $compressionlevel = [System.IO.Compression.CompressionLevel]::Optimal
 
-# A blank directory needs to be made to create the zip archive
-$test = "$here\Test"
-if(test-path $test){
-    remove-item -path $test -force
+    # Set the zip write method type
+    $update = [System.IO.Compression.ZipArchiveMode]::Update
+
+    # Get a listing of all the file paths necessary to create
+    $children = get-childitem -path $source -recurse -force
+    $count = $children.Count
+    $i = 0
 }
-else{
-    new-item -path .\ -name "Test" -itemtype Directory
-}
 
-# Set the compression level for the archive
-$compressionlevel = [System.IO.Compression.CompressionLevel]::Optimal
+process {
+    # Create the base zip archive
+    [System.IO.Compression.ZipArchive]$archive = [System.IO.Compression.ZipFile]::Open($destination,$update)
 
-# Create zip archive
-[System.IO.Compression.ZipFile]::CreateFromDirectory($test,$dest,$compressionlevel,$false)
-
-# Dispose of empyt shell folder
-remove-item -path $test -force
-
-# Get a listing of all the file paths necessary to create
-$children = get-childitem -path $source -recurse -force
-$count = $children.Count
-$count
-$i = 0
-
-$update = [System.IO.Compression.ZipArchiveMode]::Update
-[System.IO.Compression.ZipArchive]$archive = [System.IO.Compression.ZipFile]::Open($dest,$update)
-
-foreach($child in $children){
-    if($child.PSIsContainer){
-        # make a name that honors file structure
-        $path = ($($child.FullName).Replace(($source + "\"),''))
-        if($path){
-            $name = $path.Replace("\","/")
-        }
-        else{
-            $name = $child.Name
-        }
-
-        # create the directory inside the archive
-        $archive.CreateEntry("$name/")
+    while (-not(test-path $destination)) {
+        start-sleep -milliseconds 50
     }
-    else{
-        # figure out where in the structure the file should reside
-        $path = ($child.FullName).Replace(($source + "\"),'').Replace($child.Name,'')
-        if($path -match '\\'){
-            # at least one subdirectory exists
-            $name = $path.Replace("\","/") + $child.Name
-        }
-        else{
-            $name = $child.Name
+
+    foreach($child in $children){
+        if($child.PSIsContainer){
+            # make a name that honors file structure
+            $path = $child.FullName -Replace [Regex]::Escape("$source\")
+            if($path){
+                $name = $path.Replace("\","/")
+            }
+            else{
+                $name = $child.Name
+            }
+
+            # create the directory inside the archive
+            $archive.CreateEntry("$name/")
         }
 
-        [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$child.FullName,$name,$compressionlevel)
+        else{
+            # figure out where in the structure the file should reside
+            $path = $child.FullName -Replace [Regex]::Escape("$source\") -Replace "$($child.Name)"
+            if($path -match '\\'){
+                # at least one subdirectory exists
+                $name = $path.Replace("\","/") + $child.Name
+            }
+
+            else{
+                $name = $child.Name
+            }
+
+            try{
+                [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$child.FullName,$name,$compressionlevel)
+            }
+            catch{
+                # make temporary file if original is open with another process
+                $temp = get-content -path $child.FullName -raw
+                $tempFile = [System.IO.Path]::GetTempFileName()
+                $temp | out-file $tempFile
+                [System.IO.Compression.ZipFileExtensions]::CreateEntryFromFile($archive,$tempFile,$name,$compressionlevel)
+                remove-item -path $tempFile -force
+                $temp = $null; $tempFile = $null
+            }
+        }
+
         $i++
-    }
 
+    }
 }
 
-$archive.Dispose()
+end {
+    $archive.Dispose()
+}
